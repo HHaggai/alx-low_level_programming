6fc49036287dbca5c2ecac9423164b68389c2950
Hhaggai <hillhaggai@outlook.com>
2023-08-21 12:27:33 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addit
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - addit
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - addit
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - addit
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - addit
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
